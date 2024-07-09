173a69d91b2de19e963f8e1d028024755260020f
Mark Snyder <Snyder4000@yahoo.com>
2024-07-09 11:38:36 -0400

    Initial Project Setup

---
+++ banking/src/main/java/com/revature/Main.java
@@ -0,0 +1,15 @@
+package com.revature;
+
+public class Main {
+    public static void main(String[] args) {
+         /*
+            Registration Steps
+                - user needs to prompt they want to make an account
+                - user needs to provide a username and password
+                - system needs to check the username and password conform to software requirements
+                - system needs to save the credentials if they are valid, or reject them if they are not
+                - user needs to be informed of the results
+         */
+        System.out.println("Hello world!");
+    }
+}
